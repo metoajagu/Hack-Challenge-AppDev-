6c8fddcff46d293997f3a08a56e03b8145920027
METO AJAGU <jva24@cornell.edu>
2023-11-30 17:25:43 -0500

    Fixed Game, Profile and Added Ticket Struct

--- Sportify/Sportify/Game.swift
+++ Sportify/Sportify/Game.swift
@@ -13,12 +13,12 @@ struct Game: Hashable {
     let sport: String
     let gender: String
     let location: String
-    let date_time: Date
+    let dateTime: Date
     let teams: [String]
     let num_tickets: Int
     let users_attending: [Profile]
 }
 
-var dummyData: Game = Game(id: 1, sportLogo: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Cornell_Big_Red_logo.svg/640px-Cornell_Big_Red_logo.svg.png", sport: "Basketball", gender: "Men's", location: "Bartels", date_time: Date(), teams: ["Cornell", "Harvard"], num_tickets: 200, users_attending: [])
+var dummyData: Game = Game(id: 1, sportLogo: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Cornell_Big_Red_logo.svg/640px-Cornell_Big_Red_logo.svg.png", sport: "Basketball", gender: "Men's", location: "Bartels", dateTime: Date(), teams: ["Cornell", "Harvard"], num_tickets: 200, users_attending: [])
 var games: [Game] = []
 
---
+++ Sportify/Sportify/Ticket.swift
@@ -0,0 +1,8 @@
+//
+//  Ticket.swift
+//  Sportify
+//
+//  Created by Meto Ajagu on 11/30/23.
+//
+
+import Foundation
