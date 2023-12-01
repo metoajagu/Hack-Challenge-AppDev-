126bdc30c654269029dc72c229e265683206b3da
METO AJAGU <jva24@cornell.edu>
2023-12-01 00:12:18 -0500

    Added NetworkManager and added fetchRoster

--- Sportify/Sportify/Game.swift
+++ Sportify/Sportify/Game.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Game: Hashable, Identifiable {
+struct Game: Codable, Identifiable {
     let id = UUID()
     let sport: String
     let image: String
@@ -17,9 +17,11 @@ struct Game: Hashable, Identifiable {
     let awayLogo: String
     let away: String
     let num_tickets: Int
-    let users_attending: [Profile]
+    let tickets: [Ticket]
+    let usersAttending: [Profile]
 }
 
-var games: [Game] = [Game(sport: "Basketball", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Cornell_%22C%22_logo.svg/1587px-Cornell_%22C%22_logo.svg.png",gender: "Men's", location: "Bartels", dateTime: Date(), awayLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Harvard_Crimson_logo_2020.svg/640px-Harvard_Crimson_logo_2020.svg.png", away: "Harvard", num_tickets: 200, users_attending: []),
-                     Game(sport: "Hockey", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Cornell_%22C%22_logo.svg/1587px-Cornell_%22C%22_logo.svg.png", gender: "Men's", location: "Bartels", dateTime: Date(), awayLogo:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Boston_University_seal.svg/1200px-Boston_University_seal.svg.png", away:"BU", num_tickets: 100, users_attending: [])]
+var games: [Game] = [Game(sport: "Basketball", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Cornell_%22C%22_logo.svg/1587px-Cornell_%22C%22_logo.svg.png",gender: "Men's", location: "Bartels", dateTime: Date(), awayLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Harvard_Crimson_logo_2020.svg/640px-Harvard_Crimson_logo_2020.svg.png", away: "Harvard", num_tickets: 200, tickets: [], usersAttending: []),
+    Game(sport: "Hockey",
+         image:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Cornell_%22C%22_logo.svg/1587px-Cornell_%22C%22_logo.svg.png", gender: "Men's", location: "Bartels", dateTime: Date(), awayLogo:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Boston_University_seal.svg/1200px-Boston_University_seal.svg.png", away:"BU", num_tickets: 100, tickets: [], usersAttending: [])]
 
--- Sportify/Sportify/NetworkingManager.swift
+++ Sportify/Sportify/NetworkingManager.swift
@@ -5,6 +5,51 @@
 //  Created by Meto Ajagu on 11/30/23.
 //
 
-import UIKit
+import SwiftUI
+import Alamofire
+
+struct NetworkingManager {
+    
+   static let shared = NetworkingManager()
+    
+    private init() {    }
+    
+    func fetchRoster(completion: @escaping ([Game]) -> Void) {
+//        Make Endpoint URL
+        let endpoint = ""
+//        Create Parameters
+//        let parameters: Parameters = [
+//            "sport": event.sport,
+//            "sex": event.gender,
+//            "date_time": event.dateTime,
+//            "location": event.location,
+//            "away_team_logo": event.awayLogo,
+//            "away_team_name": event.away,
+//            "num_tickets": event.num_tickets,
+//            "tickets": event.tickets,
+//            "users_attending": event.usersAttending
+//        ]
+//        Create JSON Decoder
+        let decoder = JSONDecoder()
+//        Create Request
+        AF.request(endpoint, method: .get)
+            .validate()
+            .responseDecodable(of: [Game].self, decoder:decoder) {
+                response in
+                
+                switch response.result {
+                case .success(let events):
+                    print("Succesfully fetched \(events.count)")
+                    completion(events)
+                case .failure(let error):
+                    print("Error in NetworkManager.fetchRoster: \(error.localizedDescription)")
+                }
+            }
+        
+    }
+    
+}
+
+
 
 
