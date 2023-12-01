d5aa256e68500f3351ccb0744c69a74dc12ecfc1
METO AJAGU <jva24@cornell.edu>
2023-12-01 18:11:08 -0500

    Added sendRequest Function (POST)

--- Sportify/Sportify/NetworkingManager.swift
+++ Sportify/Sportify/NetworkingManager.swift
@@ -16,7 +16,7 @@ struct NetworkingManager {
     
     func fetchRoster(sport: String, completion: @escaping ([Game]) -> Void) {
 //        Make Endpoint URL
-        let endpoint = "\(sport)"
+        let endpoint = "http://34.48.20.64/games/\(sport)"
 //        Create Parameters
 //        let parameters: Parameters = [
 //            "sport": event.sport,
@@ -43,6 +43,34 @@ struct NetworkingManager {
                     completion(events)
                 case .failure(let error):
                     print("Error in NetworkManager.fetchRoster: \(error.localizedDescription)")
+                }
+            }
+        
+    }
+    
+    func sendRequest(event: Game, completion: @escaping (Game) -> Void) {
+//        Specify endpoint
+        let endpoint = "http://34.48.20.64/"
+        
+//        Create Parameters:
+        let parameters: Parameters = [
+            "sport": event.sport,
+            "sex": event.gender,
+            "location": event.location
+        ]
+//        Create decoder
+        let decoder = JSONDecoder()
+        
+        AF.request(endpoint, method: .post, parameters: parameters)
+            .validate()
+            .responseDecodable(of: Game.self, decoder: decoder) {
+                response in
+                switch response.result {
+                case .success(let event):
+                    print("Successfully added game \(event.sport), \(event.gender) and \(event.location)")
+                    completion(event)
+                case .failure(let error):
+                    print("Error in NetworkManager.sendRequest:\(error.localizedDescription)")
                 }
             }
         
